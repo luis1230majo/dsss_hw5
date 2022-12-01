104012a9ef6b8409fa4959d759d4ee61a34f8367
Luis Cervantes <luis.b.cervantes@fau.de>
2022-12-01 21:31:27 +0100

    modified init

--- snowflake/__init__.py
+++ snowflake/__init__.py
@@ -1,9 +1,1 @@
-"""
-pyexample.
-
-An example python library.
-"""
-
-__version__ = "0.1.0"
-__author__ = 'Luis Cervantes'
-__credits__ = 'Friedrich-Alexander-Universitat Erlangen-Nurnberg'
+from .let_it_snow import *
